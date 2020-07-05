2dafdeb69a2d686cdbaac51cb0edb2c2dc8f38be
Glaston D'souza <glastondsouza97@gmail.com>
2020-07-05 14:18:41 +0530

    connected to DB, handled errors and added colors package.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+// Load env file
+dotenv.config({ path: "./config/config.env" });
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
 
-// Load env file
-dotenv.config({ path: "./config/config.env" });
+// Connect to database.
+connectDB();
 
 // Initializing app
 const app = express();
@@ -19,9 +24,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} on PORT ${process.env.PORT}`
+      .yellow.bold
   )
 );
+
+// Handle unhandled promise rejections.
+process.on("unhandledRejection", (err, promise) => {
+  console.log("Error : ".red, err.message);
+  // Close server and exit process.
+  server.close(() => process.exit(1));
+});
