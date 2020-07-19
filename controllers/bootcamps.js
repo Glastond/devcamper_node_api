7388062d17729c24201efd0bbc7ff790b3c5411b
Glaston D'souza <glastondsouza97@gmail.com>
2020-07-19 18:02:08 +0530

    Added query string filtering in get all Bootcamps.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,13 +6,23 @@ const geocoder = require("../utils/geocoder");
 // @desc    Get all bootcamps.
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = asyncHandler(async (_, res, next) => {
-  const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  let query;
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
 
   res.status(200).json({
     success: true,
-    data: bootcamps,
     count: bootcamps.length,
+    data: bootcamps,
   });
 });
 
