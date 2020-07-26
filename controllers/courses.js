96f8b6b2c2ca555d1a0e14133e2b435d8c253738
Glaston D'souza <glastondsouza97@gmail.com>
2020-07-26 16:25:20 +0530

    Added funtionality of deleting Courses.

--- controllers/courses.js
+++ controllers/courses.js
@@ -86,7 +86,7 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(
-      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`, 404)
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
     );
   }
 
@@ -98,5 +98,25 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: course,
+  });
+});
+
+// @desc    Delete Course.
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
