0dc6cff4b5c302b5606528ca0a314823c11e1ae8
Glaston D'souza <glastondsouza97@gmail.com>
2020-07-26 16:18:50 +0530

    Added functionality to update course.

--- controllers/courses.js
+++ controllers/courses.js
@@ -77,3 +77,26 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+// @desc    Update Course.
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
