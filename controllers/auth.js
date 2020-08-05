579b5af00fe3711ce69173a6bf27fcccaa96fe71
Glaston D'souza <glastondsouza97@gmail.com>
2020-08-05 12:56:51 +0530

    Added functionality to generate resetPasswordToken.

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,6 +50,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Forgot password.
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse("There is no User with that Email.", 404));
+  }
+
+  // Get Reset token.
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc    Get current logged in User.
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send message.
 const sendTokenResponse = (user, statusCode, res) => {
   //Create token
@@ -71,15 +104,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   });
 };
-
-// @desc    Get current logged in User.
-// @route   POST /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,7 +3,6 @@ const Bootcamp = require("../models/Bootcamps");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
-const { Query } = require("mongoose");
 
 // @desc    Get all bootcamps.
 // @route   GET /api/v1/bootcamps
