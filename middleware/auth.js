e1d9705f9daab677c31fd89cf297487ebc67aa5b
Glaston D'souza <glastondsouza97@gmail.com>
2020-08-08 16:13:19 +0530

    Added functionality for logging users out.

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // set token from Bearer token from header.
     token = req.headers.authorization.split(" ")[1];
+
+    // Set token from Cookie.
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-  //   else if(req.cookies.token){
-  //       token = req.cookies.token
-  //   }
 
   // Make sure the token exists
   if (!token) {
