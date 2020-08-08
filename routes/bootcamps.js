0c30da78c06c61d7c900b39b16d8a5e4a474a9bd
Glaston D'souza <glastondsouza97@gmail.com>
2020-08-08 14:49:16 +0530

    Added review model and added functionality to fetch all and single review.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,6 +13,7 @@ const Bootcamp = require("../models/Bootcamps");
 
 // Include other resourses router.
 const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 const router = express.Router();
 
@@ -22,6 +23,7 @@ const { protect, authorize } = require("../middleware/auth");
 
 // Reroute into other resource router.
 router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 router.route("/radius/:zipcode/:distance/:unit").get(getBootcampsInRadius);
 
